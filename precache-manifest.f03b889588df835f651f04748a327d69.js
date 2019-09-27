29eadbea3f7c8200e82c124e4ed498c24e128abe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-27 10:59:55 +0000

    Deploy yacob89/sevenminutes-react to github.com/yacob89/sevenminutes-react.git:gh-pages

---
+++ precache-manifest.f03b889588df835f651f04748a327d69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4731ad6588102a33eda952852c3605",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1dfec37b0b3c9eab50f",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fd7df7f6e739e488d19e",
+    "url": "/static/js/2.2e9c4732.chunk.js"
+  },
+  {
+    "revision": "c1dfec37b0b3c9eab50f",
+    "url": "/static/js/main.aab18f73.chunk.js"
+  },
+  {
+    "revision": "b0c4e6110b3b5b21e0d1",
+    "url": "/static/js/runtime-main.beece12b.js"
+  }
+]);
